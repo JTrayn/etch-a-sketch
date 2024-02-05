47ae5000d96b143340371f39d9a157ad5da7efcf
Jayden Traynor <celador24510@gmail.com>
2024-02-05 18:46:53 +0800

    Create initial files
    Initial files and CSS structure set up.

---
+++ javascript.js
@@ -0,0 +1,18 @@
+/**********************
+    ETCH A SKETCH
+**********************/
+/**
+ * Author: Jayden Traynor
+ * Date: 5 February 2024
+ */
+
+const SCREEN_SIZE = 10;
+let screen = document.querySelector('.screen');
+
+for (let i = 0; i < SCREEN_SIZE; i++) {
+    
+    let row = document.createElement('div');
+    row.classList.add('row');
+    
+}
+
